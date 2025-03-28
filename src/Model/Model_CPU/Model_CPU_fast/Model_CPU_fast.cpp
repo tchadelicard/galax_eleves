d54827d7c78833118707ea64055828b81f92c30d
Tchadel Icard <tchadelicard@protonmail.com>
2025-03-28 16:23:42 +0100

    feat: add CPU fast v2 (OpenMP + XSIMD)

--- src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
+++ src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
@@ -23,40 +23,45 @@ void Model_CPU_fast
 	std::fill(accelerationsy.begin(), accelerationsy.end(), 0);
 	std::fill(accelerationsz.begin(), accelerationsz.end(), 0);
 
+	const b_type b10 = b_type(10.0f);
+	const b_type b1 = b_type(1.0f);
+
 	#pragma omp parallel for schedule(static)
 	for (int i = 0; i < n_particles; i++)
 	{
 		b_type acc_x = b_type(0.0f);
 		b_type acc_y = b_type(0.0f);
 		b_type acc_z = b_type(0.0f);
+
+		b_type xi = b_type(particles.x[i]);
+		b_type yi = b_type(particles.y[i]);
+		b_type zi = b_type(particles.z[i]);
 		for (int j = 0; j < n_particles; j += b_type::size)
 		{
-			if(i != j)
-			{
+			if (i != j) {
 				b_type xj = b_type::load_unaligned(&particles.x[j]);
 				b_type yj = b_type::load_unaligned(&particles.y[j]);
 				b_type zj = b_type::load_unaligned(&particles.z[j]);
 				b_type mj = b_type::load_unaligned(&initstate.masses[j]);
 
-				b_type xi = b_type(particles.x[i]);
-				b_type yi = b_type(particles.y[i]);
-				b_type zi = b_type(particles.z[i]);
 
 				b_type diffx = xj - xi;
 				b_type diffy = yj - yi;
 				b_type diffz = zj - zi;
 
 				b_type dij = diffx * diffx + diffy * diffy + diffz * diffz;
+				dij = xs::max(dij, b1);
 
 				// Compute inverse squared-root and handle singularities
-				auto mask = dij < b_type(1.0f);
-				dij = xs::select(mask, b_type(10.0f), xs::rsqrt(dij) * xs::rsqrt(dij) * xs::rsqrt(dij) * b_type(10.0f));
+				b_type inv_dij = xs::rsqrt(dij);
+				inv_dij = inv_dij * inv_dij * inv_dij;
+
+				dij = b10 * inv_dij;
 
 
 				acc_x += diffx * dij * mj;
 				acc_y += diffy * dij * mj;
 				acc_z += diffz * dij * mj;
-
 			}
 		}
 		// Store results back
