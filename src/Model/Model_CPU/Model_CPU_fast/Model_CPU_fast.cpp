be8cd2a7de37f67ef0e4afe72ad6e6ef72d49f3b
Tchadel Icard <tchadelicard@protonmail.com>
2025-03-28 16:20:19 +0100

    feat: add CPU fast v0 (only OpenMP)

--- src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
+++ src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
@@ -23,28 +23,46 @@ void Model_CPU_fast
     std::fill(accelerationsy.begin(), accelerationsy.end(), 0);
     std::fill(accelerationsz.begin(), accelerationsz.end(), 0);
 
-// OMP  version
-// #pragma omp parallel for
-//     for (int i = 0; i < n_particles; i ++)
-//     {
-//     }
+	#pragma omp parallel for schedule(static)
+	for (int i = 0; i < n_particles; i++)
+	{
+		for (int j = 0; j < n_particles; j++)
+		{
+			if(i != j)
+			{
+				const float diffx = particles.x[j] - particles.x[i];
+				const float diffy = particles.y[j] - particles.y[i];
+				const float diffz = particles.z[j] - particles.z[i];
 
+				float dij = diffx * diffx + diffy * diffy + diffz * diffz;
 
-// OMP + xsimd version
-// #pragma omp parallel for
-//     for (int i = 0; i < n_particles; i += b_type::size)
-//     {
-//         // load registers body i
-//         const b_type rposx_i = b_type::load_unaligned(&particles.x[i]);
-//         const b_type rposy_i = b_type::load_unaligned(&particles.y[i]);
-//         const b_type rposz_i = b_type::load_unaligned(&particles.z[i]);
-//               b_type raccx_i = b_type::load_unaligned(&accelerationsx[i]);
-//               b_type raccy_i = b_type::load_unaligned(&accelerationsy[i]);
-//               b_type raccz_i = b_type::load_unaligned(&accelerationsz[i]);
+				if (dij < 1.0)
+				{
+					dij = 10.0;
+				}
+				else
+				{
+					dij = std::sqrt(dij);
+					dij = 10.0 / (dij * dij * dij);
+				}
 
-//         ...
-//     }
+				accelerationsx[i] += diffx * dij * initstate.masses[j];
+				accelerationsy[i] += diffy * dij * initstate.masses[j];
+				accelerationsz[i] += diffz * dij * initstate.masses[j];
+			}
+		}
+	}
 
+	#pragma omp parallel for schedule(static)
+	for (int i = 0; i < n_particles; i++)
+	{
+		velocitiesx[i] += accelerationsx[i] * 2.0f;
+		velocitiesy[i] += accelerationsy[i] * 2.0f;
+		velocitiesz[i] += accelerationsz[i] * 2.0f;
+		particles.x[i] += velocitiesx   [i] * 0.1f;
+		particles.y[i] += velocitiesy   [i] * 0.1f;
+		particles.z[i] += velocitiesz   [i] * 0.1f;
+	}
 }
 
 #endif // GALAX_MODEL_CPU_FAST
