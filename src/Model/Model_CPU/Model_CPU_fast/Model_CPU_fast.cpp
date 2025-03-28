694062c59e5d5d5c9c40d435d8a82f915b17ece1
Tchadel Icard <tchadelicard@protonmail.com>
2025-03-28 16:22:14 +0100

    feat: add CPU fast v1 (OpenMP + XSIMD)

--- src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
+++ src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
@@ -19,49 +19,83 @@ Model_CPU_fast
 void Model_CPU_fast
 ::step()
 {
-    std::fill(accelerationsx.begin(), accelerationsx.end(), 0);
-    std::fill(accelerationsy.begin(), accelerationsy.end(), 0);
-    std::fill(accelerationsz.begin(), accelerationsz.end(), 0);
+	std::fill(accelerationsx.begin(), accelerationsx.end(), 0);
+	std::fill(accelerationsy.begin(), accelerationsy.end(), 0);
+	std::fill(accelerationsz.begin(), accelerationsz.end(), 0);
 
 	#pragma omp parallel for schedule(static)
 	for (int i = 0; i < n_particles; i++)
 	{
-		for (int j = 0; j < n_particles; j++)
+		b_type acc_x = b_type(0.0f);
+		b_type acc_y = b_type(0.0f);
+		b_type acc_z = b_type(0.0f);
+		for (int j = 0; j < n_particles; j += b_type::size)
 		{
 			if(i != j)
 			{
-				const float diffx = particles.x[j] - particles.x[i];
-				const float diffy = particles.y[j] - particles.y[i];
-				const float diffz = particles.z[j] - particles.z[i];
+				b_type xj = b_type::load_unaligned(&particles.x[j]);
+				b_type yj = b_type::load_unaligned(&particles.y[j]);
+				b_type zj = b_type::load_unaligned(&particles.z[j]);
+				b_type mj = b_type::load_unaligned(&initstate.masses[j]);
 
-				float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+				b_type xi = b_type(particles.x[i]);
+				b_type yi = b_type(particles.y[i]);
+				b_type zi = b_type(particles.z[i]);
 
-				if (dij < 1.0)
-				{
-					dij = 10.0;
-				}
-				else
-				{
-					dij = std::sqrt(dij);
-					dij = 10.0 / (dij * dij * dij);
-				}
+				b_type diffx = xj - xi;
+				b_type diffy = yj - yi;
+				b_type diffz = zj - zi;
 
-				accelerationsx[i] += diffx * dij * initstate.masses[j];
-				accelerationsy[i] += diffy * dij * initstate.masses[j];
-				accelerationsz[i] += diffz * dij * initstate.masses[j];
+				b_type dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+				// Compute inverse squared-root and handle singularities
+				auto mask = dij < b_type(1.0f);
+				dij = xs::select(mask, b_type(10.0f), xs::rsqrt(dij) * xs::rsqrt(dij) * xs::rsqrt(dij) * b_type(10.0f));
+
+
+				acc_x += diffx * dij * mj;
+				acc_y += diffy * dij * mj;
+				acc_z += diffz * dij * mj;
+
 			}
 		}
+		// Store results back
+		accelerationsx[i] += xs::reduce_add(acc_x);
+		accelerationsy[i] += xs::reduce_add(acc_y);
+		accelerationsz[i] += xs::reduce_add(acc_z);
 	}
 
 	#pragma omp parallel for schedule(static)
-	for (int i = 0; i < n_particles; i++)
+	for (int i = 0; i < n_particles; i += b_type::size)
 	{
-		velocitiesx[i] += accelerationsx[i] * 2.0f;
-		velocitiesy[i] += accelerationsy[i] * 2.0f;
-		velocitiesz[i] += accelerationsz[i] * 2.0f;
-		particles.x[i] += velocitiesx   [i] * 0.1f;
-		particles.y[i] += velocitiesy   [i] * 0.1f;
-		particles.z[i] += velocitiesz   [i] * 0.1f;
+		b_type axi = b_type::load_unaligned(&accelerationsx[i]);
+		b_type ayi = b_type::load_unaligned(&accelerationsy[i]);
+		b_type azi = b_type::load_unaligned(&accelerationsz[i]);
+
+		b_type pxi = b_type::load_unaligned(&particles.x[i]);
+		b_type pyi = b_type::load_unaligned(&particles.y[i]);
+		b_type pzi = b_type::load_unaligned(&particles.z[i]);
+
+		b_type vxi = b_type::load_unaligned(&velocitiesx[i]);
+		b_type vyi = b_type::load_unaligned(&velocitiesy[i]);
+		b_type vzi = b_type::load_unaligned(&velocitiesz[i]);
+
+
+		vxi += axi * b_type(2.0f);
+		vyi += ayi * b_type(2.0f);
+		vzi += azi * b_type(2.0f);
+
+		pxi += vxi * b_type(0.1f);
+		pyi += vyi * b_type(0.1f);
+		pzi += vzi * b_type(0.1f);
+
+		vxi.store_unaligned(&velocitiesx[i]);
+		vyi.store_unaligned(&velocitiesy[i]);
+		vzi.store_unaligned(&velocitiesz[i]);
+
+		pxi.store_unaligned(&particles.x[i]);
+		pyi.store_unaligned(&particles.y[i]);
+		pzi.store_unaligned(&particles.z[i]);
 	}
 }
 
